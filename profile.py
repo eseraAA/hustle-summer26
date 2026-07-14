de7c1d6845c7b6a646c12a45188aa5e5b6804303
Esera Alarqaban <eseraalarqaban@Eseras-MacBook-Pro.local>
2026-07-13 22:22:11 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,22 @@
+full_name = "Esera Alarqaban"
+
+nickname = "Esera"
+
+age = 15
+
+used_python_before = True
+
+hobbies = ["reading", "music", "coding"]
+
+favorites = {
+    "movie": "Black Panther",
+    "food": "pizza",
+    "hobby": "coding"
+}
+
+print(full_name)
+print(nickname)
+print(age)
+print(used_python_before)
+print(hobbies)
+print(favorites)
